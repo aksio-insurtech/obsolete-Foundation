426d9d27d0069b078f39b7dfafb9e2708eb6d069
Einar Ingebrigtsen <einari@me.com>
2021-09-03 14:25:18 +0200

    Adding some Xml Docs

--- Specifications/BDD/Specification.cs
+++ Specifications/BDD/Specification.cs
@@ -3,14 +3,55 @@ using System.Reflection;
 
 namespace Aksio.BDD
 {
+    /// <summary>
+    /// Represents the base class for specifications.
+    /// </summary>
+    /// <remarks>
+    /// The lifecycle of a specifiction is as follows:
+    /// - Establish : establishes the context (Given)
+    /// - Because : performs the action       (When)
+    /// - [Fact] : all your facts             (Then)
+    /// - Destroy : performs cleanup of the context
+    /// .
+    /// The different methods are by convention, meaning that having a private method called "Establish", "Destroy", "Because"
+    /// with a void signature taking no arguments will automatically be called.
+    /// All "Then" statements are considered the xUnit Facts we want to run assertions for.
+    /// .
+    /// It will recursively execute lifecycle methods in the inheritance hierarchy.
+    /// This enables one to encapsulate reusable contexts. The order it executes them in
+    /// is reversed; meaning that it will start at the lowest level in the inheritance chain
+    /// and move towards the specific type.
+    /// Example:
+    /// Context class:
+    /// public class a_specific_context : Specification
+    /// {
+    ///     void Establish() => ....
+    /// }
+    /// .
+    /// Specification class:
+    /// _
+    /// public class when_doing_something : a_specific_context
+    /// {
+    ///     void Establish() => ....
+    ///     void Because() => ....
+    ///     [Fact] It_should_do_something() => ....
+    /// }
+    /// .
+    /// It will run the Establish first for the `a_specific_context` and then the `when_doing_something`
+    /// class.
+    /// </remarks>
     public class Specification : IDisposable
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="Specification"/>.
+        /// </summary>
         public Specification()
         {
             OnEstablish();
             OnBecause();
         }
 
+        /// <inheritdoc/>
         public void Dispose()
         {
             OnDestroy();
--- Specifications/BDD/SpecificationMethods.cs
+++ Specifications/BDD/SpecificationMethods.cs
@@ -3,7 +3,33 @@ using System.Reflection;
 
 namespace Aksio.BDD
 {
-    public class SpecificationMethods<T>
+    /// <summary>
+    /// Represents the lifecycle methods for a <see cref="Specification"/>.
+    /// </summary>
+    /// <typeparam name="T">Target type it represents.</typeparam>
+    /// <remarks>
+    /// It will recursively execute lifecycle methods in the inheritance hierarchy.
+    /// This enables one to encapsulate reusable contexts. The order it executes them in
+    /// is reversed; meaning that it will start at the lowest level in the inheritance chain
+    /// and move towards the specific type.
+    /// Example:
+    /// Context class:
+    /// public class a_specific_context : Specification
+    /// {
+    ///     void Establish() => ....
+    /// }
+    /// _
+    /// Specification class:
+    /// _
+    /// public class when_doing_something : a_specific_context
+    /// {
+    ///     void Establish() => ....
+    /// }
+    /// -
+    /// It will run the Establish first for the `a_specific_context` and then the `when_doing_something`
+    /// class.
+    /// </remarks>
+    public static class SpecificationMethods<T>
     {
         static IEnumerable<MethodInfo> _establish { get; }
         static IEnumerable<MethodInfo> _because { get; }
@@ -31,8 +57,22 @@ namespace Aksio.BDD
             return methods;
         }
 
+        /// <summary>
+        /// Invoke all Establish methods.
+        /// </summary>
+        /// <param name="unit">Unit to invoke them on.</param>
         public static void Establish(object unit) => InvokeMethods(_establish, unit);
+
+        /// <summary>
+        /// Invoke all Destroy methods.
+        /// </summary>
+        /// <param name="unit">Unit to invoke them on.</param>
         public static void Destroy(object unit) => InvokeMethods(_destroy, unit);
+
+        /// <summary>
+        /// Invoke all Because methods.
+        /// </summary>
+        /// <param name="unit">Unit to invoke them on.</param>
         public static void Because(object unit) => InvokeMethods(_because, unit);
 
         static void InvokeMethods(IEnumerable<MethodInfo> methods, object unit)
