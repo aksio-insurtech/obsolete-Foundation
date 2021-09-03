073b0c59d8259badf801cb140b536222885b3044
Einar Ingebrigtsen <einari@me.com>
2021-09-03 14:25:11 +0200

    Removing statics

--- Specifications/Fundamentals/Collections/when_combining_no_lookups.cs
+++ Specifications/Fundamentals/Collections/when_combining_no_lookups.cs
@@ -3,8 +3,8 @@ namespace Aksio.Collections.for_CollectionExtensions
 {
     public class when_combining_no_lookups : Specification
     {
-        static IEnumerable<ILookup<string, int>> lookups;
-        static ILookup<string, int> result;
+        IEnumerable<ILookup<string, int>> lookups;
+        ILookup<string, int> result;
 
         void Establish() => lookups = Enumerable.Empty<ILookup<string, int>>();
 
