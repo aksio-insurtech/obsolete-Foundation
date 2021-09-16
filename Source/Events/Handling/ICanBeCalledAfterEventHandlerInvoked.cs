143ce34bdfc36f8de891fc870d03104df829190c
Einar Ingebrigtsen <einari@me.com>
2021-09-16 14:38:21 +0200

    Started looking at lifecycle / middleware for event handlers

---
+++ Source/Events/Handling/ICanBeCalledAfterEventHandlerInvoked.cs
@@ -0,0 +1,19 @@
+using Dolittle.SDK.Events;
+
+namespace Aksio.Events.Handling
+{
+    /// <summary>
+    /// Defines an interface for a callback that will be called before any of the event handlers.
+    /// </summary>
+    public interface ICanBeCalledAfterEventHandlerInvoked
+    {
+        /// <summary>
+        /// Called after every handle method is invoked on an event handler.
+        /// </summary>
+        /// <param name="eventHandler"><see cref="IEventHandler"/> that will be invoked.</param>
+        /// <param name="event">The actual event that it will be called with.</param>
+        /// <param name="eventContext"><see cref="EventContext"/> for the event.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        Task AfterInvoked(IEventHandler eventHandler, object @event, EventContext eventContext);
+    }
+}
