9ed175b98190b7510497a127f0b050354a185267
Einar Ingebrigtsen <einari@me.com>
2021-10-29 14:45:05 +0200

    Regenerated proxy

--- Sample/Web/Accounts/Debit/AllAccounts.ts
+++ Sample/Web/Accounts/Debit/AllAccounts.ts
@@ -2,15 +2,13 @@
  *  **DO NOT EDIT** - This file is an automatically generated file.
  *--------------------------------------------------------------------------------------------*/
 
-import { IQueryFor, useQuery, RefreshQuery } from '@aksio/frontend/queries';
+import { QueryFor, QueryResult, useQuery, PerformQuery } from '@aksio/frontend/queries';
 import { DebitAccount } from './DebitAccount';
 
-export class AllAccounts implements IQueryFor<DebitAccount> {
-    get route(): string {
-        return '/api/accounts/debit';
-    }
+export class AllAccounts extends QueryFor<DebitAccount> {
+    readonly route: string = '/api/accounts/debit';
 
-    static use(): [DebitAccount[], RefreshQuery] {
-        return useQuery<AllAccounts, DebitAccount>();
+    static use(): [QueryResult<DebitAccount>, PerformQuery] {
+        return useQuery<DebitAccount, AllAccounts>(AllAccounts);
     }
 }
