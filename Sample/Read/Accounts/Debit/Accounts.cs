3cdc45f9639d0dbd5e86206c0f399151405c3530
Einar Ingebrigtsen <einari@me.com>
2021-11-01 13:57:32 +0100

    Implementing a query with parameters as sample

--- Sample/Read/Accounts/Debit/Accounts.cs
+++ Sample/Read/Accounts/Debit/Accounts.cs
@@ -1,3 +1,5 @@
+using MongoDB.Bson;
+
 namespace Read.Accounts.Debit
 {
     [Route("/api/accounts/debit")]
@@ -10,7 +12,14 @@ namespace Read.Accounts.Debit
         [HttpGet]
         public IEnumerable<DebitAccount> AllAccounts() => _collection.Find(_ => true).ToList();
 
-        [HttpGet("some/{category}")]
-        public IEnumerable<DebitAccount> SomeAccounts([FromRoute] string category, [FromQuery] string? startingWith) => _collection.Find(_ => true).ToList();
+        [HttpGet("starting-with")]
+        public IEnumerable<DebitAccount> StartingWith([FromQuery] string? filter)
+        {
+            var filterDocument = Builders<DebitAccount>
+                .Filter
+                .Regex("name", $"^{filter ?? string.Empty}.*");
+
+            return _collection.Find(filterDocument).ToList();
+        }
     }
 }
