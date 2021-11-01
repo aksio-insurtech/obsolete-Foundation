5e72b2a2306caed4b4bfcbcca32eeaf56c882164
Einar Ingebrigtsen <einari@me.com>
2021-11-01 14:52:46 +0100

    Removing Foundation feature tests

--- Sample/Main/MyEventHandlersLogMessages.cs
+++
@@ -1,8 +0,0 @@
-namespace Sample
-{
-    internal static partial class MyEventHandlersLogMessages
-    {
-        [LoggerMessage(0, LogLevel.Information, "We're here : {Content}")]
-        internal static partial void MyEventHandled(this ILogger logger, int content);
-    }
-}
