2ed7ae82aefe2ce4c4ca383086af906af9bd26d3
Einar Ingebrigtsen <einari@me.com>
2021-11-15 16:02:00 +0100

    Removing DateTime for now, till we have support for it

--- Sample/Read/AccountHolders/AccountHolder.cs
+++ Sample/Read/AccountHolders/AccountHolder.cs
@@ -1,4 +1,4 @@
 namespace Read.AccountHolders
 {
-    public record AccountHolder(string FirstName, string LastName, DateTime DateOfBirth, string SocialSecurityNumber, string Address, string City, string PostalCode, string Country);
+    public record AccountHolder(string FirstName, string LastName, string SocialSecurityNumber, string Address, string City, string PostalCode, string Country);
 }
