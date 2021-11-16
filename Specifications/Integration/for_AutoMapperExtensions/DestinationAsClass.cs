645e6b199d452825b524a7ed0397556823ac6fd0
Einar Ingebrigtsen <einari@me.com>
2021-11-16 12:02:01 +0100

    Specs for AutoMapper Extensions

---
+++ Specifications/Integration/for_AutoMapperExtensions/DestinationAsClass.cs
@@ -0,0 +1,8 @@
+namespace Aksio.Integration.for_AutoMapperExtensions
+{
+    public class DestinationAsClass
+    {
+        public int DestinationInteger { get; set; }
+        public string DestinationString { get; set; }
+    }
+}
---
+++ Specifications/Integration/for_AutoMapperExtensions/when_mapping_members.cs
@@ -0,0 +1,35 @@
+using AutoMapper;
+
+namespace Aksio.Integration.for_AutoMapperExtensions
+{
+    public class when_mapping_members : Specification
+    {
+        IMapper mapper;
+
+        SourceAsClass source;
+        DestinationAsClass result;
+
+        void Establish()
+        {
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg
+                    .CreateMap<SourceAsClass, DestinationAsClass>()
+                    .MapMember(_ => _.DestinationInteger, _ => _.SourceInteger)
+                    .MapMember(_ => _.DestinationString, _ => _.SourceString);
+            });
+
+            mapper = configuration.CreateMapper();
+            source = new()
+            {
+                SourceInteger = 42,
+                SourceString = "Forty Two"
+            };
+        }
+
+        void Because() => result = mapper.Map<DestinationAsClass>(source);
+
+        [Fact] void should_map_integer_value() => result.DestinationInteger.ShouldEqual(source.SourceInteger);
+        [Fact] void should_map_string_value() => result.DestinationString.ShouldEqual(source.SourceString);
+    }
+}
