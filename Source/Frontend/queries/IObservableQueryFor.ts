c99ca8c467ec8b4be32336e477a438c7a2356ad1
Einar Ingebrigtsen <einari@me.com>
2021-11-22 15:59:26 +0100

    Defining what observable queries can be

--- Source/Frontend/queries/IObservableQueryFor.ts
+++ Source/Frontend/queries/IObservableQueryFor.ts
@@ -2,11 +2,16 @@ import { QueryResult } from './QueryResult';
 import Handlebars from 'handlebars';
 
 /**
+ * The delegate type representing the callback of result from the server.
+ */
+export type OnNextResult = <TDataType>(data: QueryResult<TDataType>) => void;
+
+/**
  * Defines the base of a query.
  * @template TDataType Type of model the query is for.
  * @template TArguments Optional type of arguments to use for the query.
  */
-export interface IQueryFor<TDataType, TArguments = {}> {
+export interface IObservableQueryFor<TDataType, TArguments = {}> {
     readonly route: string;
     readonly routeTemplate: Handlebars.TemplateDelegate;
 
@@ -15,8 +20,9 @@ export interface IQueryFor<TDataType, TArguments = {}> {
 
     /**
      * Perform the query.
+     * @param {OnNextResult} callback The callback that will receive result from the server.
      * @param [args] Optional arguments for the query - depends on whether or not the query needs arguments.
      * @returns {QueryResult} for the model
      */
-    perform(args?: TArguments): Promise<QueryResult<TDataType>>;
+    onNext(callback: OnNextResult, args?: TArguments): void;
 }
